ac0152d946721dd13145a7d31377b5e597764369
inspi-writer <conservedinnovation@gmail.com>
2024-10-08 17:02:09 +0100

    starting fix for pnl

--- app/utils/generateImage.js
+++ app/utils/generateImage.js
@@ -22,68 +22,112 @@ export const generateImage = async (
   log(pair, tradeAction, initialPrice, currentPrice);
   const percentage = calculatePercentageChange(initialPrice, currentPrice);
   const response = await axios.post(
-    "https://api.canvas.switchboard.ai/",
+    "https://rest.apitemplate.io/v2/create-image?template_id=27d77b23b5f7e5a0",
     {
-      template: "elitesniper",
-      sizes: [
+      overrides: [
         {
-          width: 1920,
-          height: 1080
-        }
-      ],
-      elements: {
-        image1: {
-          url: "https://via.placeholder.com/500/500"
+          name: "img_1",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/96be2fec-23c1-4bc3-ab96-221093d13c6c.jpg"
         },
-        PNL: {
-          text: "PNL"
-        },
-        pair_name: {
+        {
+          name: "pair",
           text: pair,
-          color: "#FFED15"
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
         },
-        percentage: {
+        {
+          name: "trade_action",
+          text: tradeAction,
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
+        },
+        {
+          name: "text_1",
           text: `${percentage.toFixed(2)}%`,
           textBackgroundColor: "rgba(0, 0, 0, 0)",
           color: isNegative(percentage) ? "#FF4D4D" : "#56E92D"
+          // color: "#56E92D"
         },
-        invested: {
-          text: "Invested"
-        },
-        text5: {
-          text: "Current Worth"
-        },
-        brock_icon: {
-          url: "https://via.placeholder.com/500/500"
-        },
-        brock_icon2: {
-          url: "https://via.placeholder.com/500/500"
-        },
-        entry_brock_amount: {
-          text: `$${initialPrice} BROCK`,
+        {
+          name: "invested",
+          text: "Invested",
           textBackgroundColor: "rgba(0, 0, 0, 0)",
-          color: "#9A9A9A"
+          color: "#FFFFFF"
         },
-        current_brock_amount: {
-          text: `$${currentPrice} BROCK`,
+        {
+          name: "text_2",
+          text: "Current value",
           textBackgroundColor: "rgba(0, 0, 0, 0)",
-          color: "#9A9A9A"
+          color: isNegative(percentage) ? "#FF4D4D" : "#56E92D"
         },
-        invested_in_usd: {
-          text: "$50.60"
+        {
+          name: "brock_logo",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/01e19f98-f031-439d-80b5-d83672ccfa63.png"
         },
-        current_price_in_usd: {
-          text: "$60.60"
+        {
+          name: "amount_bought",
+          text: `$${initialPrice}`,
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
         },
-        image4: {
-          url: "https://via.placeholder.com/500/500"
+        {
+          name: "text_3",
+          text: `${currentPrice}`,
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: isNegative(percentage) ? "#FF4D4D" : "#56E92D"
+        },
+        {
+          name: "brock_logo_1",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/01e19f98-f031-439d-80b5-d83672ccfa63.png"
+        },
+        {
+          name: "invested_value_usd",
+          text: "$50.50",
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
+        },
+        {
+          name: "current_value_usd",
+          text: "$50.50",
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: isNegative(percentage) ? "#FF4D4D" : "#56E92D"
+        },
+        {
+          name: "pnl",
+          text: "PNL",
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#D6D721"
+        },
+        {
+          name: "elite_sniper_logo",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/19040649-92ce-44ac-899b-6ff8e82f3e5b.jpg"
+        },
+        {
+          name: "bitrockelitebot",
+          text: "@bitrockelitebot",
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
+        },
+        {
+          name: "apeonbitrock",
+          text: "@apeonbitrock",
+          textBackgroundColor: "rgba(0, 0, 0, 0)",
+          color: "#FFFFFF"
+        },
+        {
+          name: "telegram_logo",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/67108168-d002-4613-b054-5459775d2ed6.png"
+        },
+        {
+          name: "telegram_logo_1",
+          src: "https://apitemplateio-user.s3-ap-southeast-1.amazonaws.com/21680/31616/67108168-d002-4613-b054-5459775d2ed6.png"
         }
-      }
+      ]
     },
     {
       headers: {
         "Content-Type": "application/json",
-        "X-API-KEY": "e605fe2b-0c7c-4c69-8ca7-fe003b210263"
+        "X-API-KEY": "5fb0MjE2ODA6MTg3OTU6VVFRUWhnZHk5aDJDSkxDTQ="
       }
     }
   );
